c2a12d7c3de694047514034419f23058f5b0378c
Naveen Naidu <naveennaidu479@gmail.com>
2024-07-25 15:08:17 +0530

    process HEAD request

--- server.c
+++ server.c
@@ -1,11 +1,17 @@
 #include <asm-generic/socket.h>
+#include <fcntl.h>
 #include <netinet/in.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
+
+// errors
+#define FILE_NOT_FOUND -2
 
 // todo: take this as an argument
 #define PORT 8082
@@ -51,31 +57,32 @@
 */
 struct HTTPRequest {
   // Method details
-  char method[4];    // "GET" or "POST"
-  char uri[25];      // "/index.html" things before '?' (used for GET request)
-  char protocol[10]; // "HTTP/1.1"
+  char method[30];    // "GET" or "POST"
+  char uri[30];      // "/index.html" things before '?' (used for GET request)
+  char protocol[30]; // "HTTP/1.1"
 
   // Headers
-  char host_hdr[20];          // "localhost:8082"
-  char authorization_hdr[20]; // <secret>
-  char content_type_hdr[20];  // "application/json"
-  int content_length_hdr;     // 38 (used for POST request)
+  char host_hdr[30];          // "localhost:8082"
+  char authorization_hdr[30]; // <secret>
+  char content_type_hdr[30];  // "application/json"
+  long content_length_hdr;    // 38 (used for POST request)
 
   // Payload
-  char paylod[BUFFER_SIZE]; // { "title":"foo","body":"bar", "id": 1}
+  char payload[BUFFER_SIZE]; // { "title":"foo","body":"bar", "id": 1}
 };
 
 struct HTTPResponse {
-  char protocol[10];  // "HTTP/1.1"
-  int status_code;    // 200
-  int status_message; // "OK"
+  char protocol[30];       // "HTTP/1.1"
+  int status_code;         // 200
+  char status_message[30]; // "OK"
 
   // Headers
-  char content_type_hdr[20]; // "application/json"
-  int content_length_hdr;    // 38 (sent back in GET response)
+  char content_type_hdr[30];  // "application/json"
+  char accept_ranges_hdr[30]; // "bytes"
+  long content_length_hdr;    // 38 (sent back in GET response)
 
   // Payload
-  char paylod[BUFFER_SIZE]; // { "title":"foo","body":"bar", "id": 1}
+  char payload[BUFFER_SIZE]; // { "title":"foo","body":"bar", "id": 1}
 };
 
 void printHTTPRequest(struct HTTPRequest *request) {
@@ -86,9 +93,64 @@ void printHTTPRequest(struct HTTPRequest *request) {
   printf("Host Header: %s\n", request->host_hdr);
   printf("Authorization Header: %s\n", request->authorization_hdr);
   printf("Content-Type Header: %s\n", request->content_type_hdr);
-  printf("Content-Length Header: %d\n", request->content_length_hdr);
-  printf("Payload: %s\n", request->paylod);
+  printf("Content-Length Header: %ld\n", request->content_length_hdr);
+  printf("Payload: %s\n", request->payload);
   printf("-------------------------------\n");
+}
+
+void printHTTPResponse(struct HTTPResponse *http_response) {
+  printf("-------------- HTTP Response -----------\n");
+  printf("%s %d %s\n", http_response->protocol, http_response->status_code,
+         http_response->status_message);
+  printf("Content-Length: %ld\n", http_response->content_length_hdr);
+  printf("Accept-Ranges: %s\n", http_response->accept_ranges_hdr);
+  printf("Content-Type: %s\n", http_response->content_type_hdr);
+  printf("Payload: %s\n", http_response->payload);
+  printf("-------------------------------\n");
+}
+
+char *construct_http_response_string(struct HTTPResponse *http_response) {
+  // Allocate a buffer for the response string
+  char *response_string = malloc(BUFFER_SIZE); // Adjust size as needed
+  if (response_string == NULL) {
+    perror("Failed to allocate memory");
+    exit(EXIT_FAILURE);
+  }
+
+  // Initialize the response string
+  response_string[0] = '\0';
+
+  // Add the status line
+  sprintf(response_string, "%s %d %s\r\n", http_response->protocol,
+          http_response->status_code, http_response->status_message);
+
+  // Add headers if they are not empty
+  if (http_response->content_length_hdr > 0) {
+    char content_length[64];
+    sprintf(content_length, "Content-Length: %ld\r\n",
+            http_response->content_length_hdr);
+    strcat(response_string, content_length);
+  }
+  if (strlen(http_response->accept_ranges_hdr) > 0) {
+    strcat(response_string, "Accept-Ranges: ");
+    strcat(response_string, http_response->accept_ranges_hdr);
+    strcat(response_string, "\r\n");
+  }
+  if (strlen(http_response->content_type_hdr) > 0) {
+    strcat(response_string, "Content-Type: ");
+    strcat(response_string, http_response->content_type_hdr);
+    strcat(response_string, "\r\n");
+  }
+
+  // End headers section
+  strcat(response_string, "\r\n");
+
+  // Add payload if it is not empty
+  if (strlen(http_response->payload) > 0) {
+    strcat(response_string, http_response->payload);
+  }
+
+  return response_string;
 }
 
 // Parse the raw HTTP request and populate the HTTPRequest struct
@@ -100,7 +162,7 @@ void parse_http_request(char *buffer, struct HTTPRequest *http_request) {
   // GET /test HTTP/1.1\r\n
   method = strtok(buffer, " ");
   uri = strtok(NULL, " ");
-  uri = uri + 1; // remove the leading '/'
+  // uri = uri + 1; // remove the leading '/'
   prot = strtok(NULL, " \r\n");
 
   strcpy(http_request->method, method);
@@ -146,9 +208,78 @@ void parse_http_request(char *buffer, struct HTTPRequest *http_request) {
   // The headers and payload body is seperated by "\r\n\r\n", hence move the
   // pointer that much forward
   payload_body = payload_body + 3;
-  strcpy(http_request->paylod, payload_body);
+  strcpy(http_request->payload, payload_body);
 }
 
+long get_file_length(char *file_name) {
+  FILE *fp = fopen(file_name, "r");
+  if (fp == NULL) {
+    return FILE_NOT_FOUND;
+  }
+  int file_descriptor = fileno(fp);
+  if (file_descriptor < 0) {
+    perror("error getting file descriptor");
+    exit(EXIT_FAILURE);
+  }
+  // Get the file attributes
+  // This allows us to get the size of file without opening it
+  struct stat file_stat;
+  fstat(file_descriptor, &file_stat);
+  return file_stat.st_size;
+}
+
+void process_head_http_request(struct HTTPRequest *http_request,
+                               struct HTTPResponse *http_response) {
+  int a;
+  // Read the file length. Store it in content length. If file not found return
+  // 404
+  long file_length = get_file_length(http_request->uri);
+  if (file_length == FILE_NOT_FOUND) {
+    http_response->status_code = 404;
+    strcpy(http_response->status_message, "Not Found");
+  } else {
+    http_response->content_length_hdr = file_length;
+    http_response->status_code = 200;
+    strcpy(http_response->status_message, "OK");
+  }
+
+  strcpy(http_response->protocol, http_request->protocol);
+  strcpy(http_response->accept_ranges_hdr, "bytes");
+  // TODO: Add the content type based on the file extension
+  strcpy(http_response->content_type_hdr, "application/octet-stream");
+}
+
+void send_http_request(int client_fd, struct HTTPResponse *http_response) {
+  char *response_string = construct_http_response_string(http_response);
+  int bytes_sent = send(client_fd, response_string, strlen(response_string), 0);
+  if (bytes_sent < 0) {
+    perror("error sending data to client");
+    exit(EXIT_FAILURE);
+  }
+}
+
+void process_http_request(struct HTTPRequest *http_request,
+                          struct HTTPResponse *http_response) {
+  // 1. Check the http method and process accordingly
+  // 2. If GET, then fetch the file and send it back
+  if (strcasecmp(http_request->method, "HEAD") == 0) {
+    process_head_http_request(http_request, http_response);
+    char *resp = construct_http_response_string(http_response);
+    printf("Response: %s\n", resp);
+  } else if (strcasecmp(http_request->method, "GET") == 0) {
+    // Send back server information
+  } else {
+    // send error response
+  }
+
+  // 1. The file name is present in the URI
+  // 2. Fetch the file using the file name
+  // 3. Add it to payload and send
+
+  return;
+}
+
+// Handle the client request
 void handle_client_request(int client_fd) {
   char *buffer = (char *)malloc(BUFFER_SIZE * sizeof(char));
   int bytes_recvd;
@@ -156,6 +287,7 @@ void handle_client_request(int client_fd) {
   char *method, *uri, *prot, *payload;
   int payload_size;
   struct HTTPRequest *http_request = malloc(sizeof(struct HTTPRequest));
+  struct HTTPResponse *http_response = malloc(sizeof(struct HTTPResponse));
 
   // Get the data from client and store it in buffer
   // FIXME: Current assumption is that, we get the entire request in a single
@@ -180,9 +312,11 @@ void handle_client_request(int client_fd) {
   // Add a null terminator to know that we have reached the end of the HTTP
   // request This is useful for getting the payload body below.
   buffer[bytes_recvd] = '\0';
-  parse_http_request(buffer, http_request);
 
+  parse_http_request(buffer, http_request);
   printHTTPRequest(http_request);
+  process_http_request(http_request, http_response);
+  send_http_request(client_fd, http_response);
 
   return;
 }
@@ -207,7 +341,7 @@ int main(int argc, char *argv[]) {
   // 2. Configure the socket
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY; // the host address
-  server_addr.sin_port = htons(PORT); // convert to network byte order
+  server_addr.sin_port = htons(PORT);       // convert to network byte order
 
   // 3. bind socket to port
   if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) <
